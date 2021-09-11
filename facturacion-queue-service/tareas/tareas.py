55ef7ef75a1372b508ed17e0132a54601d0c601b
Darwin_Moreno <d.moreno11@uniandes.edu.co>
2021-09-11 10:33:21 -0500

    Primera version del microservicio de Facturacion, ya genera log

--- facturacion-queue-service/tareas/tareas.py
+++ facturacion-queue-service/tareas/tareas.py
@@ -9,16 +9,17 @@ def reporte_queue_solicitud(operacion, payload):
   logger.info('fac-queue-reporte', 'reporte_queue_solicitud', 'solicitud recibida')
 
 
-
 @celery.task(name="reporte_queue_consulta")
 def reporte_queue_consulta(operacion, payload):
   logger.info('fac-queue-reporte', 'reporte_queue_consulta', 'solicitud recibida')
-
 
 
 @celery.task(name="registrar_servicio_medico")
 def registrar_servicio_medico(operacion, payload):
   logger.info('fac-queue', 'registrar_servicio_medico', 'solicitud recibida')
 
+@celery.task(name="generar_reporte")
+def generar_reporte(operacion, payload):
+  logger.info('fac-queue', 'generar_reporte_facturacion', 'solicitud recibida')
 
 #celery -A tareas worker -l info -Q fact_queue -E
