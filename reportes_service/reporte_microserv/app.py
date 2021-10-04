f81832f2330eafdbdce400fd6d0dc4627b58393f
Manuel Sanchez Masferrer <ma.sanchezm12@uniandes.edu.co>
2021-10-04 17:21:13 -0600

    Modify reportes ms

--- reportes_service/reporte_microserv/app.py
+++ reportes_service/reporte_microserv/app.py
@@ -4,13 +4,13 @@ from celery import Celery
 from .logger import Logger
 from flask_jwt_extended import jwt_required, create_access_token, get_jwt_identity
 
-celery_app = Celery(__name__, broker='redis://127.0.0.1:6379/0')
+# celery_app = Celery(__name__, broker='redis://127.0.0.1:6379/0')
 
-@celery_app.task(name="reporte_queue_solicitud")
+# @celery_app.task(name="reporte_queue_solicitud")
 def reporte_queue_solicitud(*args):
   pass
 
-@celery_app.task(name="reporte_queue_consulta")
+# @celery_app.task(name="reporte_queue_consulta")
 def reporte_queue_consulta(*args):
   pass
 
@@ -25,15 +25,27 @@ api = Api(app)
 class VistaReporte(Resource):
   @jwt_required()
   def post(self):
-    logs.info('reporte-service', 'post', 'enviar solicitud de reporte')
-    args = ('generar', True)
-    reporte_queue_solicitud.apply_async(args=args, queue='fact_queue')
+    # url = ''
+    # response = requests.request("POST", url=url, headers=headers)
+    # user = response.json()
+    # is_authorized = user.is_authorized()
+    is_authorized = True
+    if is_authorized:
+      logs.info('reporte-service', 'post', 'enviar solicitud de reporte')
+      args = ('generar', True)
+      reporte_queue_solicitud.apply_async(args=args, queue='fact_queue')
 
   @jwt_required()
   def get(self):
-    logs.info('reporte-service', 'get', 'consultar estado de reporte')
-    args = ('consultar', True)
-    reporte_queue_consulta.apply_async(args=args, queue='fact_queue')
+    # url = ''
+    # response = requests.request("POST", url=url, headers=headers)
+    # user = response.json()
+    # is_authorized = user.is_authorized()
+    is_authorized = True
+    if is_authorized:
+      logs.info('reporte-service', 'get', 'consultar estado de reporte')
+      args = ('consultar', True)
+      reporte_queue_consulta.apply_async(args=args, queue='fact_queue')
 
 
 api.add_resource(VistaReporte, '/reporte')
